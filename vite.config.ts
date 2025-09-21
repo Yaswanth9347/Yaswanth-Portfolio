665bd21124ccf5d309a33aa6e3510a6a7c7d7ebb
Yaswanth9347 <yaswanthyerra2025@gmail.com>
2025-09-21 23:33:52 +0530

    Fix Firebase deploy to include dist build

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
-import path from 'path';
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
 
 export default defineConfig({
+  plugins: [react()],
+  base: '/',
+  publicDir: 'public',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false
+  },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, '.'),
+      '@': path.resolve(__dirname, './src')
     }
   }
 });
